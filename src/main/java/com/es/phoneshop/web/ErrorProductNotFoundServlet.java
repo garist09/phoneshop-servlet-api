1e58b09283b5fd03ea1e38c1a331667f601deb2a
Rostislav Garist <garist09@mail.ru>
2022-02-02 21:11:44 +0300

    Implemented error pages

---
+++ src/main/java/com/es/phoneshop/web/ErrorProductNotFoundServlet.java
@@ -0,0 +1,27 @@
+package com.es.phoneshop.web;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static javax.servlet.RequestDispatcher.ERROR_EXCEPTION;
+
+public class ErrorProductNotFoundServlet extends HttpServlet {
+    private static final String path = "/WEB-INF/pages/errorProductNotFound.jsp";
+    public static final String MESSAGE = "message";
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Exception attribute = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        request.setAttribute(MESSAGE, attribute.getClass().toString());
+        request.getRequestDispatcher(path).forward(request, response);
+    }
+}
